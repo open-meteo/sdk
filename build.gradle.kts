8cb9fafe6287e9bee0cc89a5c1673800f83e00d4
patrick-zippenfenig <patrick@zippenfenig.de>
2023-10-25 19:24:45 +0200

    prepare gradle package

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     `java-library`
     `maven-publish`
     signing
-    //id("com.gradle.plugin-publish") version "1.1.0" //https://docs.gradle.org/current/userguide/publishing_gradle_plugins.html
+    id("com.gradle.plugin-publish") version "1.1.0" //https://docs.gradle.org/current/userguide/publishing_gradle_plugins.html
+    //id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
-version = "0.0.0-test"
-group = "com.openmeteo"
+version = "1.0.0"
+group = "com.open-meteo"
 
 sourceSets {
     main {
@@ -17,7 +18,7 @@ sourceSets {
 }
 
 java {
-    //withJavadocJar()
+    withJavadocJar()
     withSourcesJar()
 }
 
@@ -29,6 +30,37 @@ dependencies {
     implementation("com.google.flatbuffers:flatbuffers-java:23.5.26")
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("com.open-meteo.sdk")
+                description.set("Compiled FlatBuffers schema files for the Open-Meteo Weather API")
+                url.set("https://open-meteo.com")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/open-meteo/sdk/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("patrick-zippenfenig")
+                        name.set("Patrick Zippenfenig")
+                        email.set("patrick@zippenfenig.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/open-meteo/sdk.git")
+                    developerConnection.set("scm:git:git://github.com/open-meteo/sdk.git")
+                    url.set("https://github.com/open-meteo/sdk")
+                }
+            }
+        }
+    }
+}
+
 val signingKey: String? = providers.environmentVariable("GRADLE_SIGNING_KEY").orNull
 val signingPassword: String? = providers.environmentVariable("GRADLE_SIGNING_PASSWORD").orNull
 
@@ -36,21 +68,36 @@ signing {
     if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
         useInMemoryPgpKeys(signingKey, signingPassword)
         sign(configurations.archives.get())
+        sign(publishing.publications["mavenJava"])
     } else {
         logger.warn("The signing key and password are null. This can be ignored if this is a pull request.")
     }
 }
 
-/*gradlePlugin { 
+// https://docs.gradle.org/current/userguide/publishing_gradle_plugins.html
+gradlePlugin { 
     website = "https://open-meteo.com" 
     vcsUrl = "https://github.com/open-meteo/sdk"
     plugins { 
         create("sdk") { 
-            id = "com.openmeteo.sdk"
+            id = "com.open-meteo.sdk"
             displayName = "Open-Meteo SDK"
             description = "Compiled FlatBuffers schema files for the Open-Meteo Weather API"
-            tags = listOf("openmeteo", "weather", "api")
-            implementationClass = "com.openmeteo.sdk.ApiResponse"
+            tags = listOf("open-meteo", "weather", "api")
+            implementationClass = "com.open_meteo.sdk"
+        }
+    }
+}
+
+/*val sonatypeUsername: String? = providers.environmentVariable("SONATYPE_USERNAME").orNull
+val sonatypePassword: String? = providers.environmentVariable("SONATYPE_PASSWORD").orNull
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username = sonatypeUsername
+            password = sonatypePassword
         }
     }
 }*/
