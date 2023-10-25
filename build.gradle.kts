e1322e395c4c396d12c07d957dc48dfee9361196
patrick-zippenfenig <patrick@zippenfenig.de>
2023-10-25 17:08:46 +0200

    repare gradle integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     `java-library`
     `maven-publish`
     signing
-    //id("com.gradle.plugin-publish") version "1.1.0" https://docs.gradle.org/current/userguide/publishing_gradle_plugins.html
+    //id("com.gradle.plugin-publish") version "1.1.0" //https://docs.gradle.org/current/userguide/publishing_gradle_plugins.html
 }
 
+version = "0.0.0-test"
 group = "com.openmeteo"
 
 sourceSets {
@@ -39,3 +40,17 @@ signing {
         logger.warn("The signing key and password are null. This can be ignored if this is a pull request.")
     }
 }
+
+/*gradlePlugin { 
+    website = "https://open-meteo.com" 
+    vcsUrl = "https://github.com/open-meteo/sdk"
+    plugins { 
+        create("sdk") { 
+            id = "com.openmeteo.sdk"
+            displayName = "Open-Meteo SDK"
+            description = "Compiled FlatBuffers schema files for the Open-Meteo Weather API"
+            tags = listOf("openmeteo", "weather", "api")
+            implementationClass = "com.openmeteo.sdk.ApiResponse"
+        }
+    }
+}*/
