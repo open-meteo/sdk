b182015de8ee01ab667ed2118f832c92558ba8f2
patrick-zippenfenig <patrick@zippenfenig.de>
2023-10-26 09:22:52 +0200

    fix: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,8 +67,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/open-meteo/sdk")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GH_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GH_TOKEN")
             }
         }
     }
