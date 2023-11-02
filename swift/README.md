644ace159f57c7812b0d3e149faef7053377a636
patrick-zippenfenig <patrick@zippenfenig.de>
2023-11-02 13:49:02 +0100

    fix tests

--- swift/README.md
+++ swift/README.md
@@ -16,26 +16,32 @@ Add `OpenMeteoSdk` as a dependency to your `Package.swift`
 
 ## Usage
 
-Fetch hourly weather data and decode
+Fetch hourly weather data and decode.
 
 ```swift
 import OpenMeteoSdk
 
 let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=52.52,40.1&longitude=13.41,8.1&hourly=temperature_2m&format=flatbuffers")!
 
-let responses = try await OpenMeteoSdk.fetchWeatherApi(url: url)
+let responses = try await WeatherApiResponse.fetch(url: url)
 let response = responses[0]
 ...
 ```
 
 
-Decode `Data` to an array of `WeatherApiResponse`
+`WeatherApiResponse.fetch` is not available on Linux, but you can decode `Data` to an array of `WeatherApiResponse`.
 
 ```swift
 import OpenMeteoSdk
 
 let data = ...
 let responses = WeatherApiResponse.from(data: data)
+
+// OR Validate HTTP status code and decode potenial errors
+// data may contain `{error:true, reason: "Latitude out of range"}`
+let data = ...
+let statusCpde = 400
+let responses = try WeatherApiResponse.from(data: data, statusCode: statusCode)
 ```
 
 
--- swift/Sources/OpenMeteoSdk/OpenMeteoSdk.swift
+++ swift/Sources/OpenMeteoSdk/OpenMeteoSdk.swift
@@ -8,44 +8,67 @@ public enum OpenMeteoSdkError: Error {
     case serverError(statusCode: Int)
 }
 
-public struct OpenMeteoSdk {
+extension WeatherApiResponse {
+    #if !os(Linux)
     /// Fetch data from a given URL and decode the Open-Meteo Weather API Flatbuffers structure
     @available(macOS 12.0, *)
-    public static func fetchWeatherApi(url: URL) async throws -> [WeatherApiResponse] {
-        let data = try await fetchData(url: url)
-        return WeatherApiResponse.from(data: data)
-    }
-    
-    /// Fetch `Data` from Open-Meteo API. Decode potential error for HTTP code 400 and 429,
-    @available(macOS 12.0, *)
-    public static func fetchData(url: URL) async throws -> Data {
-        struct ErorJson: Decodable {
-            let error: Bool
-            let reason: String
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
+    public static func fetch(url: URL, session: URLSession = URLSession.shared) async throws -> [WeatherApiResponse] {
+        let (data, response) = try await session.data(from: url)
         guard let res = (response as? HTTPURLResponse) else {
             throw OpenMeteoSdkError.error(message: "response is not type HTTPURLResponse")
         }
-        if [400, 429].contains(res.statusCode) {
-            let reason = try JSONDecoder().decode(ErorJson.self, from: data).reason
-            throw OpenMeteoSdkError.error(message: reason)
-        }
-        if res.statusCode != 200 {
-            throw OpenMeteoSdkError.serverError(statusCode: res.statusCode)
-        }
-        return data
+        return try WeatherApiResponse.from(data: data, statusCode: res.statusCode)
     }
+    #endif
 }
 
 
-extension FlatBufferObject {
+/// If the API throws an error, this it the JSON error format
+struct OpenMeteoErorJson: Decodable {
+    let error: Bool
+    let reason: String
+}
+
+extension FlatBufferObject where Self: Verifiable {
+    /// Decode multiple size prefixed FlatBuffer messages from `Data`.
+    ///
+    /// Check for status code. For code 400 and 429, the error message will be decoded from `data` and thrown
+    ///
+    /// Contents of data will be copied into a `ByteBuffer` and each message keeps a reference.
+    /// Thus, memory will be correctly retrained and released
+    public static func from(data: Data, statusCode: Int) throws -> [Self] {
+        if [400, 429].contains(statusCode) {
+            let reason = try JSONDecoder().decode(OpenMeteoErorJson.self, from: data).reason
+            throw OpenMeteoSdkError.error(message: reason)
+        }
+        if statusCode != 200 {
+            throw OpenMeteoSdkError.serverError(statusCode: statusCode)
+        }
+        return getSizePrefixedMessages(bb: ByteBuffer(data: data))
+    }
+    
     /// Decode multiple size prefixed FlatBuffer messages from `Data`.
     /// Contents of data will be copied into a `ByteBuffer` and each message keeps a reference.
     /// Thus, memory will be correctly retrained and released
     public static func from(data: Data) -> [Self] {
         return getSizePrefixedMessages(bb: ByteBuffer(data: data))
+    }
+    
+    /// Verify message, before decoding
+    public static func getSizePrefixedMessagesAndVerify(bb: ByteBuffer) throws -> [Self] {
+        var bb = bb
+        var verifier = try Verifier(buffer: &bb)
+        var results = [Self]()
+        var position: Int32 = 0
+        while position < bb.size {
+            let length = bb.read(def: Int32.self, position: Int(position))
+            let uoffset = bb.read(def: UOffset.self, position: Int(position) + 4)
+            try Self.verify(&verifier, at: Int(position) + 4 + Int(uoffset), of: Self.self)
+            let result = Self.init(bb, o: position + 4 + Int32(uoffset))
+            results.append(result)
+            position += length + 4
+        }
+        return results
     }
     
     /// Decode multiple size prefixed FlatBuffer messages from a `ByteBuffer`.
