61e33f3b7ee97e1150c5387be008c27ac6bf8889
Patrick Zippenfenig <patrick@zippenfenig.de>
2023-11-03 09:55:04 +0100

    feat: Swift integration (#38)
    * basic helper functions
    
    * update tests
    
    * fix tests
    
    * update tests and readme
    
    * fix linux build

--- swift/README.md
+++ swift/README.md
@@ -4,7 +4,7 @@ Add `OpenMeteoSdk` as a dependency to your `Package.swift`
 
 ```swift
   dependencies: [
-    .package(url: "https://github.com/open-meteo/sdk.git", from: "1.0.0")
+    .package(url: "https://github.com/open-meteo/sdk.git", from: "1.5.0")
   ],
   targets: [
     .target(name: "MyApp", dependencies: [
@@ -12,3 +12,94 @@ Add `OpenMeteoSdk` as a dependency to your `Package.swift`
     ])
   ]
 ```
+
+
+## Usage
+
+Fetch weather data and form a `WeatherData` structure that can be later used.
+
+```swift
+import OpenMeteoSdk
+
+let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=52.521&longitude=13.41&hourly=temperature_2m,precipitation&daily=temperature_2m_min,temperature_2m_max&timezone=auto&format=flatbuffers")!
+let responses = try await WeatherApiResponse.fetch(url: url)
+
+/// Process first location. Add a for-loop for multiple locations or weather models
+let response = responses[0]
+
+/// Attributes for timezone and location
+let utcOffsetSeconds = response.utcOffsetSeconds
+let timezone = response.timezone
+let timezoneAbbreviation = response.timezoneAbbreviation
+let latitude = response.latitude
+let longitude = response.longitude
+
+let hourly = response.hourly!
+let daily = response.daily!
+
+struct WeatherData {
+  let hourly: Hourly
+  let daily: Daily
+  
+  struct Hourly {
+    let time: [Date]
+    let temperature2m: [Float]
+    let precipitation: [Float]
+  }
+  struct Daily {
+    let time: [Date]
+    let temperature2mMax: [Float]
+    let temperature2mMin: [Float]
+  }
+}
+
+/// Note: The order of weather variables in the URL query and the `at` indices below need to match!
+let data = WeatherData(
+  hourly: .init(
+    time: hourly.getDateTime(offset: utcOffsetSeconds),
+    temperature2m: hourly.variables(at: 0)!.values,
+    precipitation: hourly.variables(at: 1)!.values
+  ),
+  daily: .init(
+    time: daily.getDateTime(offset: utcOffsetSeconds),
+    temperature2mMax: daily.variables(at: 0)!.values,
+    temperature2mMin: daily.variables(at: 1)!.values
+  )
+)
+
+/// Timezone `gmt` is deliberately used. 
+/// By adding `utcOffsetSeconds` before, local-time is inferred
+let dateFormatter = DateFormatter()
+dateFormatter.timeZone = .gmt
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+for (i, date) in data.hourly.time.enumerated() {
+  print(dateFormatter.string(from: date))
+  print(data.hourly.temperature2m[i])
+  print(data.hourly.precipitation[i])
+}
+
+for (i, date) in data.daily.time.enumerated() {
+  print(dateFormatter.string(from: date))
+  print(data.daily.temperature2mMin[i])
+  print(data.daily.temperature2mMax[i])
+}
+```
+
+
+`WeatherApiResponse.fetch` is not available on Linux, but you can decode `Data` to an array of `WeatherApiResponse`.
+
+```swift
+import OpenMeteoSdk
+
+let data = ...
+let responses = WeatherApiResponse.from(data: data)
+
+// OR Validate HTTP status code and decode potenial errors
+// data may contain `{error:true, reason: "Latitude out of range"}`
+let data = ...
+let statusCpde = 400
+let responses = try WeatherApiResponse.from(data: data, statusCode: statusCode)
+```
+
+
---
+++ swift/Sources/OpenMeteoSdk/OpenMeteoSdk.swift
@@ -0,0 +1,101 @@
+import Foundation
+import FlatBuffers
+
+public typealias WeatherApiResponse = openmeteo_sdk_WeatherApiResponse
+
+public enum OpenMeteoSdkError: Error {
+    case error(message: String)
+    case serverError(statusCode: Int)
+}
+
+extension WeatherApiResponse {
+    #if !os(Linux)
+    /// Fetch data from a given URL and decode the Open-Meteo Weather API Flatbuffers structure
+    @available(macOS 12.0, *)
+    public static func fetch(url: URL, session: URLSession = URLSession.shared) async throws -> [WeatherApiResponse] {
+        let (data, response) = try await session.data(from: url)
+        guard let res = (response as? HTTPURLResponse) else {
+            throw OpenMeteoSdkError.error(message: "response is not type HTTPURLResponse")
+        }
+        return try WeatherApiResponse.from(data: data, statusCode: res.statusCode)
+    }
+    #endif
+}
+
+
+/// If the API throws an error, this it the JSON error format
+struct OpenMeteoErorJson: Decodable {
+    let error: Bool
+    let reason: String
+}
+
+extension FlatBufferObject where Self: Verifiable {
+    /// Decode multiple size prefixed FlatBuffer messages from `Data`.
+    ///
+    /// Check for status code. For code 400 and 429, the error message will be decoded from `data` and thrown
+    ///
+    /// Contents of data will be copied into a `ByteBuffer` and each message keeps a reference.
+    /// Thus, memory will be correctly retrained and released
+    public static func from(data: Data, statusCode: Int) throws -> [Self] {
+        if [400, 429].contains(statusCode) {
+            let reason = try JSONDecoder().decode(OpenMeteoErorJson.self, from: data).reason
+            throw OpenMeteoSdkError.error(message: reason)
+        }
+        if statusCode != 200 {
+            throw OpenMeteoSdkError.serverError(statusCode: statusCode)
+        }
+        return getSizePrefixedMessages(bb: ByteBuffer(data: data))
+    }
+    
+    /// Decode multiple size prefixed FlatBuffer messages from `Data`.
+    /// Contents of data will be copied into a `ByteBuffer` and each message keeps a reference.
+    /// Thus, memory will be correctly retrained and released
+    public static func from(data: Data) -> [Self] {
+        return getSizePrefixedMessages(bb: ByteBuffer(data: data))
+    }
+    
+    /// Verify message, before decoding
+    public static func getSizePrefixedMessagesAndVerify(bb: ByteBuffer) throws -> [Self] {
+        var bb = bb
+        var verifier = try Verifier(buffer: &bb)
+        var results = [Self]()
+        var position: Int32 = 0
+        while position < bb.size {
+            let length = bb.read(def: Int32.self, position: Int(position))
+            let uoffset = bb.read(def: UOffset.self, position: Int(position) + 4)
+            try Self.verify(&verifier, at: Int(position) + 4 + Int(uoffset), of: Self.self)
+            let result = Self.init(bb, o: position + 4 + Int32(uoffset))
+            results.append(result)
+            position += length + 4
+        }
+        return results
+    }
+    
+    /// Decode multiple size prefixed FlatBuffer messages from a `ByteBuffer`.
+    public static func getSizePrefixedMessages(bb: ByteBuffer) -> [Self] {
+        var results = [Self]()
+        var position: Int32 = 0
+        while position < bb.size {
+            let length = bb.read(def: Int32.self, position: Int(position))
+            let uoffset = bb.read(def: UOffset.self, position: Int(position) + 4)
+            let result = Self.init(bb, o: position + 4 + Int32(uoffset))
+            results.append(result)
+            position += length + 4
+        }
+        return results
+    }
+}
+
+
+extension openmeteo_sdk_VariablesWithTime {
+    /// Get an array with `Date` for each timestep. `offset` in seconds can be applied to convert to local time.
+    public func getDateTime(offset: Int32 = 0) -> [Date] {
+        return stride(
+            from: time + Int64(offset),
+            to: timeEnd + Int64(offset),
+            by: Int(interval)
+        ).map {
+            Date(timeIntervalSince1970: TimeInterval($0))
+        }
+    }
+}
