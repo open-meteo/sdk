50296ee6d3b61badd676834523d32e42dce289e0
chanhihi <chanhihi55@gmail.com>
2024-02-06 20:30:38 +0900

    fix iOS version issues

--- swift/Sources/OpenMeteoSdk/OpenMeteoSdk.swift
+++ swift/Sources/OpenMeteoSdk/OpenMeteoSdk.swift
@@ -11,6 +11,7 @@ public enum OpenMeteoSdkError: Error {
 extension WeatherApiResponse {
     #if !os(Linux)
     /// Fetch data from a given URL and decode the Open-Meteo Weather API Flatbuffers structure
+    @available(iOS 13.0.0, *)
     @available(macOS 12.0, *)
     public static func fetch(url: URL, session: URLSession = URLSession.shared) async throws -> [WeatherApiResponse] {
         let (data, response) = try await session.data(from: url)
