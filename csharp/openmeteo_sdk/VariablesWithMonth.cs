5fe537af62aaec2da07d75c95d88937fb45e8da1
patrick-zippenfenig <patrick@zippenfenig.de>
2026-01-13 18:43:52 +0100

    fix: different version for gradle and csharp again...

--- csharp/openmeteo_sdk/VariablesWithMonth.cs
+++ csharp/openmeteo_sdk/VariablesWithMonth.cs
@@ -13,7 +13,7 @@ public struct VariablesWithMonth : IFlatbufferObject
 {
   private Table __p;
   public ByteBuffer ByteBuffer { get { return __p.bb; } }
-  public static void ValidateVersion() { FlatBufferConstants.FLATBUFFERS_25_9_23(); }
+  public static void ValidateVersion() { FlatBufferConstants.Constants.FLATBUFFERS_25_2_10(); }
   public static VariablesWithMonth GetRootAsVariablesWithMonth(ByteBuffer _bb) { return GetRootAsVariablesWithMonth(_bb, new VariablesWithMonth()); }
   public static VariablesWithMonth GetRootAsVariablesWithMonth(ByteBuffer _bb, VariablesWithMonth obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
   public void __init(int _i, ByteBuffer _bb) { __p = new Table(_i, _bb); }
